c227f29508c557c9e3f3a9b2c159b62f1b6a0bcc
Dina <dinagiacobbi@gmail.com>
2024-08-14 10:07:06 -0800

    Adding module alternatives

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 //Modules - encapsulated code (only share minimum)
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data =require('./6-alternative')
 
-sayHi('Bob')
-sayHi(names.john)
-sayHi(names.peter)
+require('./7-mind-grenade')
+
+// sayHi('Susan')
+// sayHi(names.john)
+// sayHi(names.peter)
+
+
