26ec5fbd501989096f3618dc192c5945ab61c5eb
Dina <dinagiacobbi@gmail.com>
2024-08-29 13:22:22 -0800

    Adding big file for streams

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const http = require('http')
+const { createReadStream} = require('fs')
+const stream = createReadStream('./content/big.txt')
 
-// const server = http.createServer((req, res) => {
-//   res.end('Welcome')
-// })
-
-// Using Event Emitter API
-const server = http.createServer()
-// emits request event
-// subcribe to it / listen for it / respond to it
-server.on('request', (req, res) => {
-  res.end('Welcome')
+stream.on('data', (result) => {
+    console.log(result)
+    
 })
-
-server.listen(5000)
